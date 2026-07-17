9881f03915aca0f2f8ffc8922f4df896109ff4fe
hasansobirov842-prog <hasansobirov842@gmail.com>
2026-07-17 10:54:39 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
+const tg = window.Telegram.WebApp;
 
+tg.expand();
+
+const buttons = document.querySelectorAll(".grid button");
+
+buttons.forEach(button => {
+    button.addEventListener("click", () => {
+
+        const name = button.innerText;
+
+        tg.HapticFeedback.impactOccurred("light");
+
+        alert(name + " is coming soon!");
+
+    });
+});
+
+const navButtons = document.querySelectorAll(".bottom-nav button");
+
+navButtons.forEach(button => {
+    button.addEventListener("click", () => {
+
+        tg.HapticFeedback.selectionChanged();
+
+    });
+});
